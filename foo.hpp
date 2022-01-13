64386a37ffd7c0159f5e6bc35bc54d7fc842c438
Piotr Sikora <01173649@pw.edu.pl>
2022-01-13 01:19:17 +0100

    gotowe

--- foo.hpp
+++ foo.hpp
@@ -9,6 +9,8 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
+    std::for_each(people.begin(), people.end(), [](Human &n){n.birthday();});
+    std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human &n) -> char{if(n.isMonster()) return 'n'; else return 'y';});
 
     // Twoja implementacja tutaj
 
